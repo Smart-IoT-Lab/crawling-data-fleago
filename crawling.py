f5a9d2f99f19dcb441e09eb6770c5e0502adf373
blesk011 <blesk0111@gmail.com>
2019-09-18 16:59:23 +0900

    Modify Crawling class to add to print property of object

--- crawling.py
+++ crawling.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import re
+from pyprnt import prnt
 from market import Market
+from firebase import firebase
 
 
 class Crawling:
@@ -9,9 +12,22 @@ class Crawling:
     
     @staticmethod
     def create_new_session():
-        # 새로운 Session 생성
+        """
+        create new session
+        :return: session
+        """
         with requests.Session() as session:
             return session
+
+    @staticmethod
+    def remove_html_tags(data):
+        """
+        remove tag between data
+        :param data: data whtch in plain data, html tag, black(space, tag)
+        :return: plain data
+        """
+        p = re.compile(r'<.*?>')
+        return p.sub('', data).strip()
 
     def read_total_data(self):
         """
@@ -22,8 +38,8 @@ class Crawling:
 
         url = "http://seoulmarket.org/search/"
         req = self.create_new_session().get(url)
-        if req.status_code == "200" and req.ok is True:
-            print(req.text)
+        if req.status_code == 200:
+            # print(req.text)
             return req.text
         else:
             print("Connection Failed! bb")
@@ -33,13 +49,19 @@ class Crawling:
         """
         get market object from total data
         """
+        market_list = []
         soup = bs(html, 'html.parser')
-        name = soup.select("#kboard-avatar-list > div.kboard-list > div.mk_list_content > div.mk_list_content_box > "
-                           + "div.mk_item.mk-name > div")
 
+        names = [self.remove_html_tags(str(x)) for x in soup.find_all("div", {"class": "kboard-avatar-cut-strings"})]
+        districts = [self.remove_html_tags(str(x)) for x in soup.find_all("div", {"class": "mk_item mk-location"})[1:]]
+        locations = [self.remove_html_tags(str(x)) for x in soup.find_all("div", {"class": "mk_item mk-place"})[1:]]
+        event_types = [self.remove_html_tags(str(x)).split() for x in soup.find_all("div", {"class": "mk_item mk-type"})[1:]]
+        introductions = [self.remove_html_tags(str(x)) for x in soup.find_all("div", {"class": "mk_item mk-intro"})[1:]]
+        page_urls = [self.remove_html_tags(str(x)) for x in soup.find_all("div", {"class": "mk_item mk-link"})[1:]]
 
-
-        pass
+        for idx, (name, district, location, event_type, introduction, page_url) in enumerate(zip(names, districts, locations, event_types, introductions, page_urls), start=1):
+            market = Market(name, district, location, event_type, introduction, page_url)
+            market_list.append(market)
 
 
 def main():
@@ -50,7 +72,6 @@ def main():
             return
         else:
             craw.parsing_market_total_data(html)
-
 
 
 if __name__ == "__main__":
