ba3053a051347652e9ab89ef6368b886535106f9
blesk011 <blesk0111@gmail.com>
2019-09-18 16:56:38 +0900

    fixed error

--- market.py
+++ market.py
@@ -1,7 +1,11 @@
 class Market:
-    # 시장 이름, 지역구, 장소, 행사 유형, 소개, 홈페이지(url)
+    # 시장 이름, 지역구, 장소, 행사유형, 소개, 홈페이지(url)
+    # 지역구 : string type, 서울시의 구 행정구역
+    # 장소 : string type, '/' 구분 - 여러 장소에서 진행되는 행사
+    # 행사 유형 : list type, 6가지 - 예술, 마을, 먹거리, 체험, 공연, 나눔
+    #
     # todo list
-    #   1. profile image
+    #   1. add profile image
 
     def __init__(self, name, district, location, event_type , introduction, page_url):
         self.name = name
@@ -18,7 +22,7 @@ class Market:
         print("시장 이름: {}".format(self.name))
         print("지역구: {}".format(self.district))
         print("장소: {}".format(self.location))
-        print("행사 유형: {}".format(self.location))
-        print("소개: {}".format(self.event_type))
-        print("홈페이지 주소:{}".format(self.introduction))
+        print("행사 유형: {}".format(self.event_type))
+        print("소개: {}".format(self.introduction))
+        print("홈페이지 주소:{}".format(self.page_url))
 
